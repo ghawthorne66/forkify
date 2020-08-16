8060206e2fbf10377b6e6caff76d19aae37a4170
Greg Hawthorne <ghawthorne@me.com>
2020-08-15 17:07:21 -0700

    lesson 143: build the search control

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import Search from "./models/Search";
 // import Recipe from './models/Recipe';
-// import 'idempotent-babel-polyfill';
+import 'idempotent-babel-polyfill';
 import * as searchView from './views/searchView';
 import {elements} from "./views/base";
 
@@ -15,18 +15,23 @@ const state = {};
 const controlSearch = async () => {
 //get query from the view
   const query = searchView.getInput();
-  console.log//TODO
+  console.log(query)//TODO
 
   if (query) {
      //new search object and add to state
      state.search = new Search(query);
      // prepare UI for results
 
+      searchView.clearInput();
+      searchView.clearResults();
+
+
       //search for recipes
      await state.search.getResults();
 
       //render the results on UI
       console.log(state.search.result);
+      searchView.renderResults(state.search.result);
   }
 }
 
